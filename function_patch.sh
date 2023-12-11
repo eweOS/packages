85831caef893fb3f494bb81db010041d294a6223
Yukari Chiba <i@0x7f.cc>
2023-12-11 14:24:46 +0800

    [pacman] 6.0.2-9: fix _patch_ function

--- function_patch.sh
+++ function_patch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 _patch_() {
-  cd "$srcdir/$1" && shift
+  cd "$srcdir/${1:-$pkgname-$pkgver}" && shift
   for FILE in ${source[@]}; do
     if [[ $FILE == *.patch ]]; then
-      echo "patch -p1 < $srcdir/$FILE"
+      patch -p1 < $srcdir/$FILE
     fi
   done
 }
